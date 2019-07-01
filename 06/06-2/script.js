75109b134483e0262d90900f2af6d6dc03e98e8d
Alisa <50300623+sheridaniam@users.noreply.github.com>
2019-07-01 15:03:43 +0300

    Create script.js

---
+++ 06/06-2/script.js
@@ -0,0 +1,9 @@
+    
+var userAge = prompt("Сколько вам лет?");
+if(userAge % 10 == 1 && userAge != 11) {
+  alert("Вам " + userAge + " год!");
+} else if ((userAge % 10 == 2 || userAge % 10 == 3 || userAge % 10 == 4) && !(userAge >=12 && userAge <=14)) {
+  alert("Вам " + userAge + " года!");
+} else {
+  alert("Вам " + userAge + " лет!")
+}
