4e7dbc97de1805c7e6b0c60ec47505e89b6aa72e
Alisa <50300623+sheridaniam@users.noreply.github.com>
2019-07-05 16:56:54 +0300

    Update script.js

--- 10/script.js
+++ 10/script.js
@@ -18,3 +18,15 @@ while (i < degreeOfNumber) {
     result *= userNumber; 
 }
 alert (userNumber +" в степени " + degreeOfNumber + " равняется " + result);
+
+
+//do while
+var userNumber = +prompt("Введите число");  
+var degreeOfNumber = +prompt("Введите степень числа");
+var result= 1;
+var i=0;
+do {
+    i++;
+    result *= userNumber
+} while (i < degreeOfNumber);
+console.log(userNumber +" в степени " + degreeOfNumber + " равняется " + result);
