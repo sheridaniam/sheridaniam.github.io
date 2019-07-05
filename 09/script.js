77a3fed07f237efab935b7e99666ff95764bc1d6
Alisa <50300623+sheridaniam@users.noreply.github.com>
2019-07-05 10:32:41 +0300

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -1,15 +1,15 @@
 var age = +prompt("Введите ваш возраст");
 switch (age) {
     case 15:
-alert("Вы еще слишком юны");
-break;
+        alert("Вы еще слишком юны");
+        break;
     case 25 :
-alert("Вы в расцвете сил");
-break;
+        alert("Вы в расцвете сил");
+        break;
     case 80:
     case 90:
-alert( "Вы уже слишком стар для этого" );
-break;
-default :
-alert( "Мы не готовы принять вашу заявку" );
+        alert( "Вы уже слишком стар для этого" );
+        break;
+    default :
+        alert( "Мы не готовы принять вашу заявку" );
 }
